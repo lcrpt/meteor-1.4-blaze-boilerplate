0838362c2269692eaab336e4e89fcbea082a31e2
Léopold de Lassence <Leopold_Dls@leo-dls.home>
2016-04-05 00:14:18 +0200

    stable version

--- imports/startup/client/useraccounts-configuration.js
+++ imports/startup/client/useraccounts-configuration.js
@@ -38,23 +38,3 @@ AccountsTemplates.configure({
     }
   }
 });
-
-// import { TAPi18n } from 'meteor/tap:i18n';
-//
-// AccountsTemplates.configure({
-//   showForgotPasswordLink: true,
-//   texts: {
-//     errors: {
-//       loginForbidden: TAPi18n.__('Incorrect username or password'),
-//       pwdMismatch: TAPi18n.__('Passwords don\'t match'),
-//     },
-//     title: {
-//       signIn: TAPi18n.__('Sign In'),
-//       signUp: TAPi18n.__('Join'),
-//     },
-//   },
-//   defaultTemplate: 'Auth_page',
-//   defaultLayout: 'App_body',
-//   defaultContentRegion: 'main',
-//   defaultLayoutRegions: {},
-// });
--- imports/startup/server/register-api.js
+++
@@ -1,4 +0,0 @@
-// import '../../api/lists/methods.js';
-// import '../../api/lists/server/publications.js';
-// import '../../api/todos/methods.js';
-// import '../../api/todos/server/publications.js';
